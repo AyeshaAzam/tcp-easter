afa19bcdd6d59844f8363694ddc54885302df532
Amanda Dahlin <amandadahlin@UXIT.local>
2019-04-16 22:21:22 +0200

    Prepare workshop

---
+++ src/EasterEgg.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "./EasterEgg.css";
+
+class EasterEgg extends React.Component {}
+
+export default EasterEgg;
+
+{
+  /* <div>
+        <div className="container">
+          <div className="egg-upper">
+            <div className="upperCrack1"> </div>
+            <div className="upperCrack2"> </div>
+            <div className="upperCrack3"> </div>
+            <div className="upperCrack4"> </div>
+          </div>
+          <div className="egg-lower">
+            <div className="crack1"> </div>
+            <div className="crack2"> </div>
+            <div className="crack3"> </div>
+          </div>
+        </div>
+      </div> */
+}
